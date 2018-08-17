dfc7ac970309556f196bf84411c221d5a4a9e549
jack <1395093509@qq.com>
2018-08-18 00:14:34 +0800

    模板更新

--- docs/.vuepress/components/LayoutDocs.vue
+++ docs/.vuepress/components/LayoutDocs.vue
@@ -1,19 +1,24 @@
 <template>
   <div class="home">
-    <div class="hero">
+    <!-- <div class="hero">
       <img v-if="data.heroImage" :src="$withBase(data.heroImage)" alt="hero">
       <h1>{{ data.heroText || $title || 'Hello' }}</h1>
       <p class="description">
         {{ data.tagline || $description || 'Welcome to your VuePress site' }}
       </p>
+    </div> -->
+    <Content custom/>
+    <div class="tags" v-if="data.tags && data.tags.length">
+      <a class="tag" :href="`#${tag}`" v-for="tag in data.tags">{{tag}}</a>
     </div>
-    <div class="features" v-if="data.features && data.features.length">
-      <div class="feature" v-for="feature in data.features">
-        <h2>{{ feature.title }}</h2>
-        <p>{{ feature.details }}</p>
+    <div class="kit-list" v-if="data.list && data.list.length">
+      <div class="item" v-for="item in data.list">
+        <h2 v-if="item.link">
+          <NavLink :item="{link: item.link, text: item.title}" />
+        </h2>
+        <p>{{ item.desc }}</p>
       </div>
     </div>
-    <Content custom/>
     <div class="footer" v-if="data.footer">
       {{ data.footer }}
     </div>
@@ -21,24 +26,93 @@
 </template>
 
 <script>
-// import NavLink from './NavLink.vue'
+import NavLink from '@default-theme/NavLink.vue'
 
 export default {
-  // components: { NavLink },
+  components: { NavLink },
   computed: {
     data () {
-      return this.$page.frontmatter
+      const data = this.$page.frontmatter;
+      // 过滤所有数据提取对应的tags
+      data.tags = data.list && data.list[0] && data.list[0].tags || [];
+      // data.tags.unshift('全部');
+      return data
     },
     actionLink () {
       return {
         link: this.data.actionLink,
-        text: this.data.actionText
+        text: this.data.actionText,
       }
     }
   }
 }
 </script>
 
-<style lang="stylus">
+<style lang="stylus" scoped>
+@import "~@default-theme/styles/config.styl"
 
+.tags
+  .tag
+    appearance none
+    display inline-flex
+    justify-content center
+    align-items center
+    white-space nowrap
+    user-select none
+    cursor pointer
+    border 1px solid transparent
+    color inherit
+    // border-radius 0.25em
+    // box-shadow 0 0 0 0.125em rgba(32, 156, 238, 0.25)
+
+  .tag + .tag
+    margin-left 1em
+
+  a.tag:active
+  a.tag:focus
+    color $accentColor
+    margin-bottom -2px
+    border-bottom 2px solid $accentColor
+
+.kit-list
+  padding 1.2rem 0
+  // margin-top 2.5rem
+  display flex
+  flex-wrap wrap
+  // align-items flex-start
+  align-content stretch
+  justify-content space-between
+
+  .item
+    box-sizing border-box
+    flex-grow 1
+    flex-basis 30%
+    max-width 30%
+    // border 1px solid $borderColor
+    margin-bottom 2em
+    padding 0 1em
+    border-radius 0.25em
+    background lighten($borderColor, 10%)
+
+    h2
+      font-size 1.4rem
+      font-weight 500
+      border-bottom none
+      padding-bottom 0
+      color lighten($textColor, 10%)
+    p
+      color lighten($textColor, 25%)
+@media (max-width: $MQMobile)
+  .list-docs
+    flex-direction column
+    .item
+      max-width 100%
+      padding 0 2.5rem
+
+@media (max-width: $MQMobileNarrow)
+  .list-docs
+    .item
+      h2
+        font-size 1.25rem
+</style>
 </style>
--- docs/topic/README.md
+++ docs/topic/README.md
@@ -1,3 +1,18 @@
-# 说明
+---
+layout: LayoutDocs
+sidebar: false
+list:
+- title: HTML
+  logo: icons/node-logo.svg
+  link: /topic/html/
+- title: CSS
+  logo: icons/node-logo.svg
+  link: /topic/css/
+- title: JavaScript
+  logo: icons/node-logo.svg
+  link: /topic/javascript/
+---
 
-可以按照标签、话题分类知识点
+# 知识点滴
+
+无论知识点还是文档，可以按照标签、话题分类知识点
